650a8ff19c8357dc65ff459a0ea0f4b35b037d17
Daniel Heck <danheck@web.de>
2017-12-31 13:52:41 +0100

    argument '...' for BayesFactorSlope()

--- R/BFslope.R
+++ R/BFslope.R
@@ -1,7 +1,7 @@
 #' Bayes Factor for Slope Parameters in Latent-Trait MPT
 #'
 #' Uses the Savage-Dickey method to compute the Bayes factor that the slope
-#' parameter is zero vs. positive/negative/unequal to zero.
+#' parameter of a continuous covariate is zero vs. positive/negative/unequal to zero.
 #'
 #' @param fittedModel a fitted latent-trait model with predictor variables that have
 #'     been defined via \code{predStructure}.
@@ -9,6 +9,9 @@
 #' @param direction alternative hypothesis: whether slope is smaller or larger than zero (\code{"<"} or \code{">"})
 #'     or unequal to zero (\code{"!="})
 #' @param plot if \code{TRUE}, the prior and posterior densities and the ratio at slope=0 are plotted.
+#' @param ... further arguments passed to \code{\link[logspline]{logspline}}, which is used to
+#'     approximate the density of the posterior distribution.
+#'
 #' @examples
 #' \dontrun{
 #' # fit beta-MPT model for encoding condition (see ?arnold2013):
@@ -30,10 +33,13 @@
 ######## Bayes factors:
 ## H0: slope beta = 0
 ## H1: slope beta < 0   (i.e., beta ~ Cauchy)
-BayesFactorSlope <- function (fittedModel, parameter, direction = "<", plot = TRUE){
+BayesFactorSlope <- function (fittedModel, parameter,
+                              direction = "<", plot = TRUE, ...){
   if (any(fittedModel$mptInfo$hyperprior$IVprec != "dchisq(1)"))
     stop("BayesFactorSlope requires that default priors are used for the slope parameter!")
 
+  if (length(parameter)!= 1 || !parameter %in% rownames(fittedModel$mcmc.summ))
+    stop("'parameter' not in model or not of length=1.")
   tmp <- strsplit(parameter, "_")
   tmp[[1]][3] <- paste(tmp[[1]][-c(1:2)], collapse = "_")
   tmp <- tmp[[1]][1:3]
@@ -49,16 +55,16 @@ BayesFactorSlope <- function (fittedModel, parameter, direction = "<", plot = TR
   if (direction == "<"){
     sel <- samples < 0
     xlim <- c(min(samples[sel], -.05), 0)
-    posterior <- logspline(samples[sel], ubound = 0)
+    posterior <- logspline(samples[sel], ubound = 0, ...)
   } else if (direction == ">"){
     sel <- samples > 0
     xlim <- c( 0, max(samples[sel], .05))
     # knots <- seq(0, 3, .5)
-    posterior <- logspline(samples[sel], lbound = 0)
+    posterior <- logspline(samples[sel], lbound = 0, ...)
   } else if (direction == "!="){
     sel <- rep(TRUE, length(samples))
     xlim <- c(min(samples[sel], -.05), max(samples[sel], .05))
-    posterior <- logspline(samples[sel])
+    posterior <- logspline(samples[sel], ...)
   } else {
     stop("'direction' must be '>', '<', or '!=' ")
   }
