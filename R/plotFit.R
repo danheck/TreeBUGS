2b9ccce27d45058857421700b6e613e4e04a4612
Daniel Heck <danheck@web.de>
2018-05-14 14:24:00 +0200

    plotFit

--- R/plotFit.R
+++ R/plotFit.R
@@ -1,13 +1,15 @@
-
 #' Plot Posterior Predictive Mean Frequencies
 #'
-#' Plots observed mean frequencies and boxplot of mean frequencies sampled from posterior distribution (posterior predictive).
+#' Plots observed means/covariances of individual frequencies against the means/covariances sampled from the posterior distribution (posterior predictive distribution).
 #'
 #' @inheritParams posteriorPredictive
 #' @param stat whether to plot mean frequencies (\code{"mean"}) or covariances of individual frequencies (\code{"cov"})
 #' @param ... arguments passed to \code{\link{boxplot}}
 #'
-#' @details If posterior predictive p-values were computed when fitting the model (e.g., by adding the argument \code{traitMPT(...,ppp=1000)} ), the stored posterior samples are re-used for plotting. Moreover, posterior samples can be added to a model after fitting as shown in the examples.
+#' @details If posterior predictive p-values were computed when fitting the model
+#' (e.g., by adding the argument \code{traitMPT(...,ppp=1000)} ), the stored posterior samples are re-used for plotting.
+#' Note that the last category in each MPT tree is dropped, because one category per multinomial distribution is fixed.
+#'
 #' @examples
 #' \dontrun{
 #' # add posterior predictive samples to fitted model:
@@ -20,11 +22,17 @@
 #' @export
 plotFit <- function(fittedModel, M=1000, stat = "mean", ...){
 
+  stat <- match.arg(stat, c("mean", "cov"))
+
   # get information about model:
   tree <- fittedModel$mptInfo$MPT$Tree
   cats <- fittedModel$mptInfo$MPT$Category
   dat <- fittedModel$mptInfo$dat[,cats]
   TreeNames <- unique(tree)
+
+  # free categories (drop last category per tree):
+  free_cats <- unlist(tapply(X = cats, INDEX = tree,
+                             FUN = function(cat) cat[-length(cat)]))
 
   # get posterior predictive:
   if(is.null(fittedModel$postpred) | M != 1000){
@@ -34,27 +42,30 @@ plotFit <- function(fittedModel, M=1000, stat = "mean", ...){
   }
 
   if(stat == "mean"){
-
     # Plot mean frequencies:
+
     pred <- t(sapply(freq.list, colMeans))
-    boxplot(pred[,cats], xaxt="n", col="gray",
+    boxplot(pred[,free_cats], xaxt="n", col="gray",
             main="Observed (red) and predicted (boxplot) mean frequencies", las=1, ...)
-    axis(1, 1:ncol(dat), labels = cats)
+    axis(1, 1:length(free_cats), labels = free_cats)
     xx <- by(1:length(tree), tree, mean)
     axis(1, xx,  TreeNames, tick=F, line=NA, mgp=c(3, 2.5, 0))
-    points(1:ncol(dat), colMeans(dat)[cats], col="red", cex=1.4, pch=17)
-    abline(v=cumsum(table(tree))[1:(length(TreeNames)-1)]+.5, col="gray")
+    points(1:length(free_cats), colMeans(dat)[free_cats], col="red", cex=1.4, pch=17)
+    abline(v=cumsum(table(tree) - 1)[1:(length(TreeNames)-1)]+.5, col="gray")
 
-  }else{
-
+  } else if (stat == "cov"){
     # Plot covariance of frequencies:
-    c.obs <- cov(dat[,cats])
+
+    nams <- outer(free_cats, free_cats, paste, sep="-")
+    sel_cov <- nams[upper.tri(nams, diag = TRUE)]
+    K <- length(sel_cov)
+
+    # observed/predicted
+    c.obs <- cov(dat[,free_cats])
     c.pred <- sapply(freq.list, function(xx){
-      cc <- cov(xx[,cats])
+      cc <- cov(xx[,free_cats])
       cc[upper.tri(cc, diag=TRUE)]
     })
-    nams <- outer(rownames(c.obs), colnames(c.obs), paste, sep="-")
-    K <- sum(upper.tri(c.obs, diag=TRUE))
 
     boxplot(t(c.pred), col="gray", ylab="Covariance",
             main="Observed (red) and predicted (gray) covariances",
@@ -64,21 +75,6 @@ plotFit <- function(fittedModel, M=1000, stat = "mean", ...){
     points(1:K, c.obs[upper.tri(c.obs, diag=TRUE)], col=2, pch=17)
     abline(v = cumsum(seq(nrow(c.obs), 2, -1))+.5, col="lightgray")
   }
-
-  # pred.mean <- colMeans(pred)
-  # pred.95 <- apply(pred, 2, quantile, probs = c((1-ci)/2, 1- (1-ci)/2))
-  # maxi <- max(pred.mean, pred.95, colMeans(dat))
-  #
-  # # Plot:
-  # plot(pred.mean, pch=15, col="gray30",ylim=c(0, maxi),xaxt="n",cex=1, las=1,
-  #      xlab="", ylab = "Mean frequencies",
-  #      main="Observed (red) and predicted (gray) mean frequencies", ...)
-  # segments(x0 = 1:ncol(pred.95), y0 = pred.95[1,], y1 = pred.95[2,], lwd = 2)
-  # axis(1, 1:ncol(dat), labels = colnames(dat))
-  # xx <- by(1:length(tree), tree, mean)
-  # axis(1, xx,  TreeNames, tick=F, line=NA, mgp=c(3, 2.5, 0))
-  # points(1:ncol(dat)+.1, colMeans(dat), col="red", cex=1, pch=16)
-  # abline(v=cumsum(table(tree))[1:(length(TreeNames)-1)]+.5)
 }
 
 
